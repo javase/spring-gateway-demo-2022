29699013a2e327abf6d215dfa209997aba6493be
5820-李孟华 <limenghua@wondersgroup.com>
2022-05-06 09:31:46 +0800

    GateWay断言

---
+++ src/test/java/com/hua/gateway/GetAfterTimeDemo.java
@@ -0,0 +1,14 @@
+package com.hua.gateway;
+
+import java.time.ZonedDateTime;
+
+/**
+ * created at 2022-05-05 11:03
+ * @author lerry
+ */
+public class GetAfterTimeDemo {
+	public static void main(String[] args) {
+		ZonedDateTime zonedDateTime = ZonedDateTime.now();
+		System.out.println(zonedDateTime);
+	}
+}
